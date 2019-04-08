20c6f935f7f9e17301e78f62c41745bd87485089
kevin-Laptop <kevin.acuna@alumnos.uneatlantico.es>
2019-04-08 16:08:19 +0200

    portada estructura

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
       name: "Web page",
       icons: [
         {
-          "src": path.resolve(__dirname, 'public/favicon.ico'),
+          "src": path.resolve(__dirname, 'public/faviconSS.ico'),
           "sizes": "64x64 32x32 24x24 16x16",
           "type": "image/x-icon"
         }
