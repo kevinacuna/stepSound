1f6f1dddd28090993ece15de732d79e8574a3807
kevin acuña <kevin.acuna@alumnos.uneatlantico.es>
2019-04-08 20:09:55 +0200

    quienes somos componente

--- src/equipo/equipo.jsx
+++ src/equipo/equipo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../public/img/hero_2.jpg';
 
 export default class Equipo extends Component {
   constructor(props) {
@@ -7,8 +8,59 @@ export default class Equipo extends Component {
   
   render() {
     return (
-      <div>
-        Quienes somos
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-md-5 text-left">
+            <h2 className="section-title mb-3">Nuestro <br/> Equipo</h2>
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus minima neque tempora reiciendis.</p>
+          </div>
+        </div>
+        <div className="row">
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div class="team-member">
+              <figure>
+                <img src="hero_2.jpg" alt="Image" class="img-fluid"/>
+              </figure>
+              <div class="p-3">
+                <h3>Kaiara Spencer</h3>
+                <span class="position">Product Manager</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div class="team-member">
+              <figure>
+                <img src="hero_2.jpg" alt="Image" class="img-fluid"/>
+              </figure>
+              <div class="p-3">
+                <h3>Kaiara Spencer</h3>
+                <span class="position">Product Manager</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div class="team-member">
+              <figure>
+                <img src="hero_2.jpg" alt="Image" class="img-fluid"/>
+              </figure>
+              <div class="p-3">
+                <h3>Kaiara Spencer</h3>
+                <span class="position">Product Manager</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4">
+            <div class="team-member">
+              <figure>
+                <img src="hero_2.jpg" alt="Image" class="img-fluid"/>
+              </figure>
+              <div class="p-3">
+                <h3>Kaiara Spencer</h3>
+                <span class="position">Product Manager</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
