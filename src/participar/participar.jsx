c264f85711d3f3a1fc1700ff0d3e1888a2a0fb15
Kevin Acuña <kevin.acuna@alumnos.uneatlantico.es>
2019-04-30 18:07:23 +0200

    como participar centrar titulo

--- src/participar/participar.jsx
+++ src/participar/participar.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Participar extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <div className="container">
-        <div className="row mb-5">
-          <div className="col-md-5 text-left">
-            <h2 className="section-title mb-3">¿Como Participar?</h2>
-            <p className="lead">Para participar primeramente deberas seguirnos en nuestras redes sociales.</p>
-            <p className="lead">subiremos contenido semanalmente para dar pistas sobre las preguntas.</p>
-            <p className="lead">Una vez adentro deberas contestar correctamente cada una de las preguntas para llegar a la estrella.</p>
-            <p className="lead">comparte con tus amigos esta increible aventura.</p>
-          </div>
+        <h2 className="text-center section-title mb-3">¿Cómo participar?</h2>
+        <hr className="divider my-4" />
+        <div className="row">
+          <p className="lead">
+            Para participar primeramente deberas seguirnos en nuestras redes
+            sociales.
+          </p>
+          <p className="lead">
+            subiremos contenido semanalmente para dar pistas sobre las
+            preguntas.
+          </p>
+          <p className="lead">
+            Una vez adentro deberas contestar correctamente cada una de las
+            preguntas para llegar a la estrella.
+          </p>
+          <p className="lead">
+            comparte con tus amigos esta increible aventura.
+          </p>
         </div>
       </div>
     );
