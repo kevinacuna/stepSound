eaf67d09d03c4dad282f498b22f8d534c04cae5f
Eabol <elder.bolcaal@alumnos.uneatlantico.es>
2019-05-01 19:58:57 +0200

    Cambios en estilo

--- src/portada/Portada.jsx
+++ src/portada/Portada.jsx
@@ -20,30 +20,40 @@ export default class Portada extends Component {
     return (
       <div className="masthead">
         <div
-          className="site-blocks-cover overlay Img-portada"
+          className="site-blocks-cover overlay base-portada"
           data-aos="fade"
           id="home-section"
         >
           <div className="container">
-            <div className="row h-100 align-items-center justify-content-center text-center">
+            <div className="row h-100 align-items-center justify-content-center text-center img-portada">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">
+                <h1 className="text-uppercase blue-grey-text text-darken-4 font-weight-bold">
                   Step Sound
                 </h1>
                 <hr className="divider my-4" />
               </div>
+
+              <div className="col-lg-6 justify-content-center text-center">
+                <p className="font-weight-light mb-5 blue-grey-text text-darken-3 text-shadow">
+
+                Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web - 
+                Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web -
+                Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web -
+                Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web</p>
+              </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web- Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web- Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web -Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web Aquí habrá una pequeña descripción de la pagina web - Aquí habrá una pequeña descripción de la pagina web</p>
-                <p className="text-white-75 font-weight-light mb-5">
+                <p className="blue-grey-text text-darken-3 font-weight-light mb-5">
                   ¡No pauses tus sueños!
                 </p>
                 <button
                   onClick={this.btnHandler}
-                  className="btn btn-primary mr-2 mb-2"
-                >
+                  className="btn btn-primary mr-2 mb-2">
                   Empezar el viaje
                 </button>
               </div>
+              
+
+              
             </div>
           </div>
         </div>
