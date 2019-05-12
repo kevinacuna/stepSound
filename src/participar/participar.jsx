6a85a747a5415a9e67e09aee37412f7f02e8df72
rodrigo <rodrigojosezaldanacalles@gmail.com>
2019-05-12 23:09:21 +0200

    como participar estilo

--- src/participar/participar.jsx
+++ src/participar/participar.jsx
@@ -11,40 +11,78 @@ export default class Participar extends Component {
         <h1 className="text-center section-title mb-3">¿Cómo participar?</h1>
         <hr className="divider my-4" />
         <div className="row">
-          <h2>
+          <h2 className="text-center col-sm-12">
             <b>Instrucciones</b>
             <br/>
             ¡BIENVENIDO A STEP SOUND!
           </h2>
           <p className="lead">
-            Acabas de unirte a una aventura inigualable, en la cual para lograr que Pablo llegue a la meta debes
-            guiarte con las siguientes instrucciones:
+            <br/>
+            Acabas de unirte a una nueva aventura en la que descubrirás una apasionante historia llena de música 
+            y lugares extraordinarios. Para ello deberás seguir los pasos que se mencionan a continuación:
+          </p>
+          <div class="col" id="c1">
+          <div id="p1">
+            <b><strong><center>Paso 1:</center></strong></b>
+          </div>
+          <div id="c1"> 
+            <br/>
+          <p className="lead">
+          Dar click en la opción <strong>“Comenzar”</strong> e inmediatamente aparecerá en la pantalla el camino que 
+          tendrás que recorren dividido en diez preguntas para llegar a la meta.
+          </p>
+          </div>
+          </div>
+          <div class="col" id="c2">
+          <div id="p2">
+            <b><strong><center>Paso 2:</center></strong></b>
+            </div>
+          <div id="c2"> 
+            <br/>
+          <p className="lead">
+          Dar click en la <strong>“Pregunta 1”.</strong> En ese momento se abrirá una nueva pestaña con una pregunta general. 
+          Esta pestaña incluye contenido multimedia y diferentes opciones para responder a la pregunta. 
+          Si no estás seguro de la respuesta escoge la opción de pista y te dirigirá a nuestras 
+          redes sociales con ayuda para responder.
+          </p>
+          </div>
+          </div>
+          <div class="col" id="c3"> 
+          <div id="p3">
+            <b><strong><center>Paso 3:</center></strong></b> 
+            </div>
+          <div id="c3"> 
+            <br/>
+          <p className="lead">
+          Para acertar la respuesta no hay tiempo límite. Así que tomate tu tiempo. No te preocupes si te equivocas 
+          siempre habrá una segunda oportunidad para poder continuar. Una vez acertada deberás de dar a la 
+          opción de “siguiente” y te dirigirá a la “Pregunta 2”. 
           </p>
           <p className="lead">
-            <b>Paso 1: </b>
-            Dar click en la opción “Comenzar” e inmediatamente te aparecerá en la
-            pantalla el camino que tendrás que recorren con una serie de pasos con preguntas para llegar a la meta.
+          Recuerda que es un camino el que has de recorrer y sin la Pregunta 1 no podrás avanzar a la siguiente.
+          </p>
+          </div>
+          </div>
+          <div class="col" id="c4"> 
+          <div id="p4">
+            <b><strong><center>Paso 4:</center></strong></b> 
+            </div>
+          <div id="c4"> 
+            <br/>
+          <p className="lead">
+          A medida que vayas avanzando las preguntas irán aumentando su dificultad. Por lo tanto te 
+          deseamos mucha suerte y ánimo en esta aventura. 
           </p>
           <p className="lead">
-            <b>Paso 2: </b>
-            Dar click en la “Pregunta 1”, y te aparecerá la pregunta, debes de responder pregunta por pregunta
-            ya que es una secuencia continua y sin la respuesta no puedes avanzar.
-            Para conseguir acertarla aparecerá un video con la historia de Pablo, dos fotos y una pista en las redes sociales.
-            Solo si aciertas podrás seguir avanzando en esta aventura.
+          Ten en cuenta que debes estar pendiente de nuestras redes sociales, 
+          ya que por medio de ellas subiremos contenido exclusivo en el cual te brindaremos pistas 
+          para que puedas lograr llegar a la meta de una manera más fácil y divertida.
           </p>
+          </div>
+          </div>
           <p className="lead">
-            <b>Paso 3: </b> Una vez que entres en cada paso, no tendrás tiempo límite para responder la pregunta,
-            así que no te preocupes tendrás el tiempo suficiente para poder acertar. 
-            Por otro lado, si no aciertas y fallas podrás seguir intentándolo después de dar click al botón de ayuda,
-            y podrás continuar hasta conseguirlo. Y después avanzar.
-          </p>
-          <p className="lead">
-            <b>Paso 4: </b> A medida que vayas avanzando las preguntas irán aumentando su dificultad,
-            por lo tanto te deseamos mucha suerte y ánimos en esta aventura. 
-          </p>
-          <p className="lead">
-            Ten en cuenta que debes estar pendiente de nuestras redes sociales, ya que por medio de ellas subiremos
-            contenido exclusivo en el cual te brindaremos pistas para que puedas lograr llegar a la meta de una manera más fácil y divertida.
+          No olvides echar un vistazo en la opción “contenido exclusivo” puesto que incluirá contenido interesante acerca de 
+          cantantes y grupos de música que pueden servirte de inspiración.
           </p>
         </div>
       </div>
