e01a07782147a232cae781085bdf8d02f61a13e4
Kevin Acuña <kevin.acuna@alumnos.uneatlantico.es>
2019-05-15 13:34:30 +0200

    modal informativo de contenido exclusivo bloqueado

---
+++ src/exclusivo/exclusivoDisabled.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default () => (
+  <div id="exclusivoDisabled">
+    <h1>
+      Para desbloquear esta función necesitas primero
+      terminar el recorrido de Pablo...
+      <br/>
+      Ve, responde todas las preguntas y luego vuelve para ver
+      contenido exclusivo; te esperaremos.
+    </h1>
+  </div>
+);
