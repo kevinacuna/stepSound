69e3b16921db9ab0d70c01c8e5605c81b11f5297
kevin-Laptop <kevin.acuna@alumnos.uneatlantico.es>
2019-05-16 14:49:37 +0200

    estilo de portada video y texto abajo

--- src/portada/portada.jsx
+++ src/portada/portada.jsx
@@ -27,34 +27,35 @@ export default class Portada extends Component {
           <div className="container">
           <img src="portada.jpg" className="img-fluid" alt="Responsive image"/>
             <div className="row h-100 align-items-center justify-content-center text-center animated fadeIn slow">
-            <div className="col-lg-6">
-              <video class="responsive-video" controls>
-                <source src="Intro.mp4" type="video/mp4"/>
-              </video>
-             </div>
-              <div className="col-lg-6 justify-content-center text-center">
-                <div className="row h-100 align-items-center justify-content-center">
-                  <div className="col-lg-12 align-self-end">
-                    <h1 className="text-uppercase blue-grey-text text-darken-4 font-weight-bold">
+            <div className="col-lg-12 align-self-end">
+                    <h1 style={{ paddingTop: '50px' }} className="text-uppercase blue-grey-text text-darken-4 font-weight-bold">
                       Step Sound
                     </h1>
                     <hr className="divider my-3" />
                   </div>
+            <div className="col-lg-12">
+              <video class="responsive-video" style={{ maxWidth: '75%' }} controls>
+                <source src="Intro.mp4" type="video/mp4"/>
+              </video>
+            </div>
+                <div className="row h-100 align-items-center justify-content-center">
                   <div className="col-lg-12 align-self-end">
-                     <p className="font-weight-light blue-grey-text text-justify text-darken-3 mt-4">
+                     <p className="font-weight-light blue-grey-text text-center text-darken-3 mt-4">
                       La historia comienza cuando el abuelo recuerda que tenía que dejar un libro a su nieto.
+                      <br/>
                       Resulta que en ese libro había una foto donde aparece el abuelo en un misterioso lugar
                       cuando era jóven, concretamente su lugar favorito.
                       </p>
-                      <p className="font-weight-light mb-5 blue-grey-text text-justify text-darken-3">
+                      <p className="font-weight-light blue-grey-text text-center text-darken-3 mt-4">
                         A modo de sorpresa, el nieto pone en marcha una aventura para encontrar el lugar
                         y poder fotografiarse allí, como un día lo hizo su abuelo.
+                        <br/>
                         Desde el momento que emprende la búsqueda lo hace acompañado de sus cascos...
                       </p>
                   </div>
-                   <div className="col-lg-6 align-self-baseline">
-                      <p className="blue-grey-text text-darken-3 font-weight-light mb-5">
-                        ¡No pauses tus sueños!
+                   <div className="col-lg-12 align-self-baseline">
+                      <p className="blue-grey-text text-darken-3 font-weight-light mb-3">
+                        <h2>¡No pauses tus sueños!</h2>
                       </p>
                       <button
                         onClick={this.btnHandler}
@@ -66,7 +67,6 @@ export default class Portada extends Component {
               </div>
             </div>
           </div>
-        </div>
     );
   }
 }
