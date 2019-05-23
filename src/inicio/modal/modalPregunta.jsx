f044880dd503117e9367d99a569ac1154818eb5c
kevin acuña <kevin.acuna@alumnos.uneatlantico.es>
2019-05-23 17:02:20 +0200

    modal pregunta responsive

--- src/inicio/modal/modalPregunta.jsx
+++ src/inicio/modal/modalPregunta.jsx
@@ -40,7 +40,7 @@ export default ({
         </div>
         <div className="modal-body">
           <div id="contenedorVideo">
-              <iframe style={{ width: 420, height: 315 }} src={data.video.url} />
+              <iframe id="video" src={data.video.url} />
           </div>
           <div id="contenedorAudio">
                 <audio src={data.audio} style={{width: '420px'}} preload="auto" controls />
